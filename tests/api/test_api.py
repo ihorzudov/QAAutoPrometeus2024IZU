cf8082b381139c6287aeb28c6fedc0abeccdba74
Ihor Zu <iamzudik@gmail.com>
2024-05-10 12:50:13 +0300

    Week 9 Module 14 Project task2

---
+++ tests/api/test_api.py
@@ -0,0 +1,19 @@
+import pytest
+
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ""
+    assert user.name == ""
+@pytest.mark.check
+def test_name():
+    assert 7 * 7 == 49
+
+@pytest.mark.check
+def test_check_math78(user):
+    assert 7 * 8 == 56
+    assert user.name == "Sergii"
+
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == "Butenko"
